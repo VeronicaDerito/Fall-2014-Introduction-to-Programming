82c3541113383c992d02f417bb6e278151e7b05d
VeronicaDerito <vderito2@gmail.com>
2014-12-01 11:43:20 -0500

    Project 5.3
    Cannot fix the error "LocaleArray is not defined"

--- KarkarogGame.js
+++ KarkarogGame.js
@@ -11,22 +11,63 @@
       var visitedRoom8 = false;
       var visitedRoom9 = false;
       var visitedRoom10 = false;
-      var hasTakenSpray = false;
-      var hasTakenKey   = false;
+ 
       function init() {
          look();
       }
      
      
+      
+      // Item Prototype
+    
+      function Item(_id, _name, _hasTaken)  {
+         this.id = _id;
+         this.name = _name;
+         
+      
+      
+      // Item Instances
+      
+      var ItemSpray = new Item ();
+      ItemSpray.id = 6;
+      ItemSpray.name = "Insect Spray";
+      ItemSpray.hasTaken = false;
+    
+      
+      var ItemKey = new Item ();
+      ItemKey.id = 10;
+      ItemKey.name = "Real Key";
+      ItemKey.hasTaken = false;
+      
+      var ItemSpoon = new Item ();
+      ItemSpoon.id = 8;
+      ItemSpoon.name = "Spoon";
+      ItemKey.hasTaken = false;
+ 
+      
+      var ItemMap = new Item ();
+      ItemMap.id = 7;
+      ItemMap.name = "Map";
+      ItemKey.hasTaken = false;
+    
+      
+      var ItemArray = [];
+           ItemArray[6] = ItemSpray;
+           ItemArray[7] = ItemMap;
+           ItemArray[8] = ItemSpoon;
+           ItemArray[10] = ItemKey;
+      
+      
+      
+   
       // Locale Prototype    
          
       function Locale(_id, _name, _description) {
          this.id = _id;
          this.name = _name;
          this.description = _description;
-      }
-      
-      
+         this.hasItem = _hasItem
+      }   
       
       
    // Locale Instances
@@ -35,96 +76,119 @@
       locale_0.id = 0;
       locale_0.name = "Main Cavern";
       locale_0.description = "You enter a main cavern with passages leading to the north, south, east, and west.";
+      locale_0.hasItem = false;
       
       
+     
      var locale_1 = new Locale();
       locale_1.id = 1;
       locale_1.name = "Chest Room";
       locale_1.description = "You enter the north entrance and  end up in a room with a massive chest. It is locked.";
+      locale_1.hasItem = false;
       
       
      var locale_2 = new Locale();
       locale_2.id = 2;
       locale_2.name = "Library";
       locale_2.description = "You enter the west entrance and find a large library.";
+      locale_2.hasItem = false;
       
      var locale_3 = new Locale();
       locale_3.id = 3;
       locale_3.name = "Wasp Room";
       locale_3.description = "You enter the south entrance and end up in a room of wasps.";
+      locale_3.hasItem = false;
       
      var locale_4 = new Locale();
       locale_4.id = 4;
       locale_4.name = "Laboratory";
       locale_4.description = "You enter the east entrance and find yourself in an abandoned laboratory.";
+      locale_4.hasItem = false;
 
      var locale_5 = new Locale();
       locale_5.id = 5;
       locale_5.name = "Hive Room";
       locale_5.description = "You head further south and enter a room with a massive hive.";
+      locale_5.hasItem = false;
       
       var locale_6 = new Locale();
       locale_6.id = 6;
       locale_6.name = "Supply Closet";
       locale_6.description = "You enter a small supply room to the south that is stocked with insect killer. You might want to take some, it may come in handy.";
+      locale_6.hasItem = true;
       
       var locale_7 = new Locale();
       locale_7.id = 7;
       locale_7.name = "Map Room";
       locale_7.description = "You pass through a door to the north and find an old classroom. There is a map on one of the desks.";
+      locale_7.hasItem = true;
       
      var locale_8 = new Locale();
       locale_8.id = 8;
       locale_8.name = "Item Room";
       locale_8.description = "You enter a room to the east and find a room full of spoons!";
+      locale_8.hasItem = true;
       
      var locale_9 = new Locale();
       locale_9.id = 9;
       locale_9.name = "Secret Room";
       locale_9.description = "You have found a secret passage! Continue on if you dare.";
+      locale_9.hasItem = false;
       
       var locale_10 = new Locale();
       locale_10.id = 10;
       locale_10.name = "Key Room";
       locale_10.description = "You found a key! This one looks promising. Grab it!";
+      locale_10.hasItem = true;
       
-      // Need to implement CURRENT LOCATION
-       var locArray = [];
-           locArray[0] = locale_0;
-           locArray[1] = locale_1;
-           locArray[2] = locale_2;
-           locArray[3] = locale_3;
-           locArray[4] = locale_4;
-           locArray[5] = locale_5;
-           locArray[6] = locale_6;
-           locArray[7] = locale_7;
-           locArray[8] = locale_8;
-           locArray[9] = locale_9;
-           locArray[10] = locale_10;
+      
+       var LocaleArray = [];
+           LocaleArray[0] = locale_0;
+           LocaleArray[1] = locale_1;
+           LocaleArray[2] = locale_2;
+           LocaleArray[3] = locale_3;
+           LocaleArray[4] = locale_4;
+           LocaleArray[5] = locale_5;
+           LocaleArray[6] = locale_6;
+           LocaleArray[7] = locale_7;
+           LocaleArray[8] = locale_8;
+           LocaleArray[9] = locale_9;
+           LocaleArray[10] = locale_10;
+           
+           
+      
+      
+           
+     
+ 
+     
       
       function btnGo_click() {
          if (txtCommand.value === "n" || txtCommand.value === "N") {
                goNorth();
          } else if (txtCommand.value === "s" || txtCommand.value === "S") {
-            display (locArray[3].description)
                goSouth();
          } else if (txtCommand.value === "e" || txtCommand.value === "E") {
-            display (locArray[2].description)
                goEast();
          } else if (txtCommand.value === "w" || txtCommand.value === "W") {
-            display (locArray[4].description)
                goWest();
          } else if (txtCommand.value === "h" || txtCommand.value === "H") {
               getHelp();
+         }else if (txtCommand.value === "i" || txtCommand.value === "I")  {
+              btnInv_click();
          } else if ((txtCommand.value === "take") && (currentLocation === 6)) { 
-            takeSpray();
+            updateInv();
          } else if ((txtCommand.value === "take") && (currentLocation === 10)) {
-            takeKey();
+            updateInv();  
+         } else if ((txtCommand.value === "take") && (currentLocation === 7)) {
+            updateInv();
+         } else if ((txtCommand.value === "take") && (currentLocation === 8)) {
+            updateInv();
          }
          else displayMessage("Invalid command, dummy! Use N, S, E, W or H for HELP!");
             
          }
-   
+   }
          
       function btnNorth_click() {   
          goNorth();
@@ -142,9 +206,25 @@
          goWest();
       }
       
-      function btnInv_click() {
-         displayInv();
+      function updateInv() {
+          if (LocaleArray[currentLocation].hasItem === false) { 
+            inventory = ""; 
+       }  else if (LocaleArray[currentLocation].hasItem === true) {
+            ItemArray[currentLocation].hasTaken = true;
+            inventory = ItemArray[currentLocation].name
+         } 
+         
       }
+      function btnInv_click() {
+         updateInv();
+         var msg = "Inventory:" + inventory;
+         display(msg);
+         
+      }
+
+      
+    
+
       
       function goNorth() {
 
@@ -349,7 +429,7 @@
          }
       }
 
-      function display(description){
+      function display(description) {
          checkScore();
          var targetTextArea = document.getElementById("taMain");
          targetTextArea.value = description + "\n\n" + "Score:" + Score + "\n\n" + targetTextArea.value; 
@@ -359,147 +439,16 @@
       function getHelp() {
          var desc = "Use the commands 'S' or 's' to go South, 'N' or 'n' to go North, 'E' or 'e' to go East, and 'W' or 'w' to go West. Use the 'take' command to pick up any items you come across. Keep track of your location with the handy map. Good luck!"
         display(desc);
-      }   
-//        function takeSpray() {
-//         var desc = "You take the insect spray."
-//         displayMessage(desc);
-//            hasTakenSpray = true;
-//   }
-//      function takeKey() {
-//         var desc = "You take the key."
-//         displayMessage(desc);
-//            hasTakenKey = true;
-//      }
-      
-//      function displayInv() {
-//         if (hasTakenSpray === true) {
-//               desc = "Spray"
-//         } else if (hasTakenSpray === false) {
-//               desc = "Inventory is empty."
-//         } else if (hasTakenKey === true)    {
-//               desc = "" + "Key"
-//         } else if (hasTakenKey === false) {
-//               desc = "Inventory is empty."
-//         } 
-//         displayMessage(desc);
-//         } 
-      
-    
-//      function mainCavern() {
-//         var desc = "You enter the main cavern with passages leading to the north, south, east, and west.";
-//         displayMessage(desc);
-//            document.getElementById("btnWest").disabled = false;
-//            document.getElementById("btnNorth").disabled = false;
-//           document.getElementById("btnEast").disabled = false;
-//            document.getElementById("btnSouth").disabled = false;
-//      }
-      
-//      function chestRoom() {
- //        var desc = "You enter the north entrance and  end up in a room with a massive chest. It is locked.";
- //        displayMessage(desc);
- //           document.getElementById("btnWest").disabled = true;
- //           document.getElementById("btnNorth").disabled = true;
- //           document.getElementById("btnEast").disabled = true;
- //           document.getElementById("btnSouth").disabled = false;
- //     }
-      
-//      function libraryRoom () {
-//         var desc = "You enter the west entrance and find a large library.";
-//         displayMessage(desc);
- //           document.getElementById("btnWest").disabled = true;
- //           document.getElementById("btnNorth").disabled = true;
-  //          document.getElementById("btnEast").disabled = false;
-  //          document.getElementById("btnSouth").disabled = false;
-         
-   //   }
-      
- //     function waspRoom() {
-  //       var desc = "You enter the south entrance and end up in a room of wasps.";
-  //       displayMessage(desc);
-  //          document.getElementById("btnWest").disabled = true;
-  //          document.getElementById("btnNorth").disabled = false;
-  //          document.getElementById("btnEast").disabled = true;
-   //         document.getElementById("btnSouth").disabled = false;
-      
-      
-   //   }
-  //    
-      
-      
- //     function hiveRoom() {
- //        var desc = "You head further south and enter a room with a massive hive.";
- //        displayMessage(desc);
- //           document.getElementById("btnWest").disabled = true;
- //           document.getElementById("btnNorth").disabled = false;
- //           document.getElementById("btnEast").disabled = true;
- //           document.getElementById("btnSouth").disabled = true;   
-  //    
-   //   }
-      
- //     function oldLab() {
-  //       var desc = "You enter the east entrance and find yourself in an abandoned laboratory.";
- //        displayMessage(desc);
- //           document.getElementById("btnWest").disabled = false;
- //           document.getElementById("btnNorth").disabled = false;
- //           document.getElementById("btnEast").disabled = false;
- //           document.getElementById("btnSouth").disabled = false;
-      
-      
-      
-  //    }
-      
- //     function supplyCloset() {
- //        var desc = "You enter a small supply room to the south that is stocked with insect killer. You might want to take some, it may come in handy.";
- //        displayMessage(desc);
- //           document.getElementById("btnWest").disabled = true;
- //           document.getElementById("btnNorth").disabled = false;
- //           document.getElementById("btnEast").disabled = true;
- //           document.getElementById("btnSouth").disabled = true;
-  //    }
-      
- //     function mapRoom() {
- //        var desc = "You pass through a door to the north and find an old classroom. There is a map on one of the desks.";
- //        displayMessage(desc);
- //           document.getElementById("btnWest").disabled = true;
- //           document.getElementById("btnNorth").disabled = true;
- //           document.getElementById("btnEast").disabled = true;
- //           document.getElementById("btnSouth").disabled = false;
-      
-  //    }
-      
- //     function itemRoom() {
- //        var desc = "You enter a room to the east and find a room full of spoons!";
- //        displayMessage(desc);
- //           document.getElementById("btnWest").disabled = false;
- //           document.getElementById("btnNorth").disabled = true;
- //           document.getElementById("btnEast").disabled = true;
- //           document.getElementById("btnSouth").disabled = true;
-  //    }
-      
-  //    function secretRoom() {
-  //       var desc = "You have found a secret passage! Continue on if you dare.";
-  //       displayMessage(desc);
-  //          document.getElementById("btnWest").disabled = true;
-  //          document.getElementById("btnNorth").disabled = false;
-  //          document.getElementById("btnEast").disabled = true;
-  //          document.getElementById("btnSouth").disabled = false;
-      
-      
-  //    }
-      
-  //    function keyRoom() {
-  //       var desc = "You found a key! This one looks promising. Grab it!";
-  //       displayMessage(desc);
-  //          document.getElementById("btnWest").disabled = true;
-  //          document.getElementById("btnNorth").disabled = false;
-  //          document.getElementById("btnEast").disabled = true;
-  //          document.getElementById("btnSouth").disabled = true;
-  //    }
-      
-   function currentLoc () {
-      display(locArray[currentLocation].description)
+  
       }
       
+     function currentLoc () {
+      display(LocaleArray[currentLocation].description)
+      } 
+     
+    
+
+ 
    
       function look() {
          var description = "";
@@ -526,12 +475,11 @@
                   break;
             case 10: currentLoc();
                   break;
-            default: desc = "You can't go that way!";      
+            default: description = "You can't go that way!";      
          }
       }   
   
       
-    
      
       
 
