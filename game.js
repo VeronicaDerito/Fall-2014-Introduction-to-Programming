623d79d7e3d783418a3e8fe1f575754a0c30b04b
VeronicaDerito <vderito2@gmail.com>
2014-12-07 15:54:59 -0500

    Project 6.6
    Buttons disable properly. Implemented new functions:
    enableNorth,enableSouth,enableEast, enableWest and used the existing
    newLoc and locale matrix

--- game.js
+++ game.js
@@ -20,7 +20,7 @@
      
      
       
-      // Item Prototype
+   // Item Prototype
     
       function Item(id,name,hasTaken)  {
          this.id 
@@ -29,14 +29,13 @@
       }   
       
       
-      // Item Instances
+   // Item Instances
       
       var ItemSpray = new Item ();
       ItemSpray.id = 6;
       ItemSpray.name = "Insect Spray";
       ItemSpray.hasTaken = false;
     
-      
       var ItemKey = new Item ();
       ItemKey.id = 10;
       ItemKey.name = "Key";
@@ -47,7 +46,6 @@
       ItemSpoon.name = "Spoon";
       ItemKey.hasTaken = false;
  
-      
       var ItemMachete = new Item ();
       ItemMachete.id = 7;
       ItemMachete.name = "Machete";
@@ -65,10 +63,9 @@
            ItemArray[10] = ItemKey;
            ItemArray[5]  = ItemRealKey;
       
-      
-      
+         
    
-      // Locale Prototype    
+   // Locale Prototype    
          
       function Locale(_id, _name, _description) {
          this.id 
@@ -148,10 +145,25 @@
       locale_10.description = "You found a key! This one looks promising. Grab it!";
       locale_10.hasItem = true;
       
+      var locArray = [];
+           locArray[0] = locale_0;
+           locArray[1] = locale_1;
+           locArray[2] = locale_2;
+           locArray[3] = locale_3;
+           locArray[4] = locale_4;
+           locArray[5] = locale_5;
+           locArray[6] = locale_6;
+           locArray[7] = locale_7;
+           locArray[8] = locale_8;
+           locArray[9] = locale_9;
+           locArray[10] = locale_10; 
+      
       var NORTH = 0;
       var SOUTH = 1;
       var EAST  = 2;
       var WEST  = 3;
+      
+   // Navigation Matrix
       
       var nav = [ // N   S   E  W
          /* 0 */  [  1,  3,  4,  2 ],
@@ -167,22 +179,8 @@
          /* 10 */ [  9, -1, -1, -1 ]
                ];
       
-       var locArray = [];
-           locArray[0] = locale_0;
-           locArray[1] = locale_1;
-           locArray[2] = locale_2;
-           locArray[3] = locale_3;
-           locArray[4] = locale_4;
-           locArray[5] = locale_5;
-           locArray[6] = locale_6;
-           locArray[7] = locale_7;
-           locArray[8] = locale_8;
-           locArray[9] = locale_9;
-           locArray[10] = locale_10;           
+   // Navigation Buttons            
 
-       var inventory = new Array();
-           
-           
       function btnGo_click() {
          if (txtCommand.value === "n" || txtCommand.value === "N") {
                goNorth();
@@ -200,12 +198,9 @@
               takeItem(); 
          } else if (txtCommand.value === "use")  {
               useItem();
-         }
-           else display("Invalid command, dummy! Use N, S, E, W or H for HELP!");
+         } else display("Invalid command, dummy! Use N, S, E, W or H for HELP!");
             
-         }
-      
-   
+      }
          
       function btnNorth_click() {   
          goNorth();
@@ -222,6 +217,73 @@
       function btnWest_click() {
          goWest();
       }
+      
+      function btnInv_click() {
+         updateInv();
+         var msg = "Inventory:" + inventory;
+         display(msg);
+      }
+      
+      function goNorth() {
+         nextLoc(NORTH);
+         enableNorth();
+         enableSouth();
+         enableEast();
+         enableWest();
+        
+        look();
+      }
+      
+      function goSouth() {
+         nextLoc(SOUTH);
+         enableNorth();
+         enableSouth();
+         enableEast();
+         enableWest();
+         look();
+      }
+         
+      function goEast() {
+         nextLoc(EAST);
+         enableNorth();
+         enableSouth();
+         enableEast();
+         enableWest();
+         
+         look();
+      }
+       
+      function goWest() {
+         nextLoc(WEST);
+         enableNorth();
+         enableSouth();
+         enableEast();
+         enableWest();
+          
+         look();
+      }
+      
+      function currentLoc() {
+      display(locArray[currentLocation].description)
+      } 
+      
+      function display(description) {
+         checkScore();
+         var targetTextArea = document.getElementById("taMain");
+         targetTextArea.value = description + "\n\n" + "Score:" + Score + "\n\n" + targetTextArea.value;
+         document.getElementById("txtCommand").focus(); 
+      }
+
+      function nextLoc(dir) {
+         var newLoc = nav[currentLocation][dir];
+         if (newLoc >= 0) {
+            currentLocation = newLoc;
+         } else display("You cannot go that way.")
+      }
+ 
+   // Inventory   
+      
+      var inventory = new Array();
       
       function updateInv() {
           if (locArray[currentLocation].hasItem) { 
@@ -241,20 +303,16 @@
           openChest();
           } else if ((currentLocation === 1) && (ItemRealKey.hasTaken === false) && (ItemKey.hasTaken === true)) {
           display ("This appears to be a fake key! Too bad.")
-          }
-           else display("You cannot use anything here!")
-          }
-      
-     
-      
-      function btnInv_click() {
-         updateInv();
-         var msg = "Inventory:" + inventory;
-         display(msg);
+          } else display("You cannot use anything here!")
       }
       
+      function getHelp() {
+         var desc = "Use the commands 'S' or 's' to go South, 'N' or 'n' to go North, 'E' or 'e' to go East, and 'W' or 'w' to go West. Use the 'take' command to pick up any items you come across and the 'use' command to use these items when appropriate. Keep track of your location with the handy map. Good luck!"
+        display(desc);
+  
+      }
+    
       function takeItem() {
-         
          switch(currentLocation) {
             case 0: updateInv();
                   break;
@@ -281,150 +339,9 @@
             default: description = "There is no item to take here!"
             }
       }
-      
-    
-      function killHive () {
-         if (hiveDefeated === true) {
-         updateInv();
-         } else display ("There is no item to take here!")
-         }
-         
-      
-      function goNorth() {
-        nextLoc(NORTH); 
-        look();
-      }
- //             document.getElementById("btnWest").disabled = true;
- //             document.getElementById("btnNorth").disabled = true;
- //             document.getElementById("btnEast").disabled = true;
- //             document.getElementById("btnSouth").disabled = false;
-              
-            
- //             document.getElementById("btnWest").disabled = true;
- //             document.getElementById("btnNorth").disabled = true;
- //             document.getElementById("btnEast").disabled = true;
- //             document.getElementById("btnSouth").disabled = true;
-         
-         
-//              document.getElementById("btnWest").disabled = true;
-//              document.getElementById("btnNorth").disabled = true;
-//              document.getElementById("btnEast").disabled = true;
-//              document.getElementById("btnSouth").disabled = false;
-         
-        
- //             document.getElementById("btnWest").disabled = true;
- //             document.getElementById("btnNorth").disabled = false;
- //             document.getElementById("btnEast").disabled = true;
- //             document.getElementById("btnSouth").disabled = false;
-         
-         
- //             document.getElementById("btnWest").disabled = false;
- //             document.getElementById("btnNorth").disabled = false;
- //             document.getElementById("btnEast").disabled = false;
- ///             document.getElementById("btnSouth").disabled = false;
-      
- //             document.getElementById("btnWest").disabled = true;
- //             document.getElementById("btnNorth").disabled = false;
- //             document.getElementById("btnEast").disabled = true;
-  //            document.getElementById("btnSouth").disabled = false;
-         
-         
- //             document.getElementById("btnWest").disabled = true;
- //             document.getElementById("btnNorth").disabled = true;
- //             document.getElementById("btnEast").disabled = false;
- //             document.getElementById("btnSouth").disabled = false;
-         
-         
-  //            document.getElementById("btnWest").disabled = true;
-  //            document.getElementById("btnNorth").disabled = true;
-  //            document.getElementById("btnEast").disabled = true;
-  //            document.getElementById("btnSouth").disabled = false;
-              
-           
-                  
-      
-      
-      function goSouth() {
-         nextLoc(SOUTH);
-         look();
-      }
-         
-         
- //             document.getElementById("btnWest").disabled = true;
- //             document.getElementById("btnNorth").disabled = false;
- //             document.getElementById("btnEast").disabled = true;
- //             document.getElementById("btnSouth").disabled = false;
-      
-         
- //             document.getElementById("btnWest").disabled = false;
- //             document.getElementById("btnNorth").disabled = false;
- //             document.getElementById("btnEast").disabled = false;
- //             document.getElementById("btnSouth").disabled = false;
-                 
-      
- //             document.getElementById("btnWest").disabled = true;
- //             document.getElementById("btnNorth").disabled = false;
- //             document.getElementById("btnEast").disabled = true;
- //             document.getElementById("btnSouth").disabled = true;
-      
-         
- //             document.getElementById("btnWest").disabled = true;
- //             document.getElementById("btnNorth").disabled = false;
- //             document.getElementById("btnEast").disabled = true;
- //             document.getElementById("btnSouth").disabled = true;
-        
-      
- //             document.getElementById("btnWest").disabled = true;
- //             document.getElementById("btnNorth").disabled = false;
- //             document.getElementById("btnEast").disabled = true;
- //             document.getElementById("btnSouth").disabled = false;
-      
-        
- //             document.getElementById("btnWest").disabled = true;
-  //            document.getElementById("btnNorth").disabled = false;
- //             document.getElementById("btnEast").disabled = true;
- //             document.getElementById("btnSouth").disabled = true;
-      
-         
- //             document.getElementById("btnWest").disabled = false;
- //             document.getElementById("btnNorth").disabled = false;
- //             document.getElementById("btnEast").disabled = false;
- //             document.getElementById("btnSouth").disabled = false;
-      
-         
-         
-        
-         
-      
-      
-      function goEast() {
-         nextLoc(EAST);
-         look();
-      }
 
-//              document.getElementById("btnWest").disabled = false;
-//              document.getElementById("btnNorth").disabled = false;
-//              document.getElementById("btnEast").disabled = false;
-//              document.getElementById("btnSouth").disabled = false;
-        
-//              document.getElementById("btnWest").disabled = false;
-//              document.getElementById("btnNorth").disabled = false;
-//              document.getElementById("btnEast").disabled = false;
-//              document.getElementById("btnSouth").disabled = false;
-      
-//              document.getElementById("btnWest").disabled = false;
-//              document.getElementById("btnNorth").disabled = true;
-//              document.getElementById("btnEast").disabled = true;
-//              document.getElementById("btnSouth").disabled = true;
-       
-       
-      function goWest() {
-         nextLoc(WEST);
-         look();
-      }
-      
-        function checkScore() {
-       if ( (! visitedRoom1) && (currentLocation === 1) ) {
+      function checkScore() {
+          if ( (! visitedRoom1) && (currentLocation === 1) ) {
             Score = Score + 5;
             visitedRoom1 = true;
          
@@ -458,35 +375,34 @@
          }
       }
 
-      function display(description) {
-         checkScore();
-         var targetTextArea = document.getElementById("taMain");
-         targetTextArea.value = description + "\n\n" + "Score:" + Score + "\n\n" + targetTextArea.value;
-         document.getElementById("txtCommand").focus(); 
-      }
-
-      
-      function getHelp() {
-         var desc = "Use the commands 'S' or 's' to go South, 'N' or 'n' to go North, 'E' or 'e' to go East, and 'W' or 'w' to go West. Use the 'take' command to pick up any items you come across and the 'use' command to use these items when appropriate. Keep track of your location with the handy map. Good luck!"
-        display(desc);
-  
-      }
-      
-     
-     
-    function currentLoc() {
-      display(locArray[currentLocation].description)
-      } 
-
-      function nextLoc(dir) {
-         var newLoc = nav[currentLocation][dir];
+      function enableNorth(dir) {
+         var newLoc = nav[currentLocation][NORTH];
          if (newLoc >= 0) {
-            currentLocation = newLoc;
-         } else {
-            display("You cannot go that way.")
-         }
+           document.getElementById("btnNorth").disabled = false;
+        } else  document.getElementById("btnNorth").disabled = true;
+      }
+      
+      function enableSouth(dir) {
+         var newLoc = nav[currentLocation][SOUTH];
+         if (newLoc >= 0) {
+         document.getElementById("btnSouth").disabled = false;
+        } else  document.getElementById("btnSouth").disabled = true; 
+      }
+           
+      function enableEast(dir) {
+         var newLoc = nav[currentLocation][EAST];
+         if (newLoc >= 0) {
+         document.getElementById("btnEast").disabled = false;
+         } else document.getElementById("btnEast").disabled = true; 
       }   
-   
+      
+      function enableWest(dir) {
+         var newLoc = nav[currentLocation][WEST];
+         if (newLoc >= 0) {
+         document.getElementById("btnWest").disabled = false;
+        } else  document.getElementById("btnWest").disabled = true;
+      }
+         
       function look() {
         switch(currentLocation) {
             case 0: currentLoc();
@@ -515,12 +431,18 @@
          }
       }   
    
-      
+   function killHive () {
+      if (hiveDefeated === true) {
+      updateInv();
+      } else display ("There is no item to take here!")
+   }
+  
    function defeatHive() {
-        hiveDefeated = true;
-        locale_5.hasItem = true;
-        display("You douse the hive with spray until the wasp colony is completely eradicated. You use the machete to cut the hive in half and find a golden key. Gross, but cool. Take it!");
-      }
+      hiveDefeated = true;
+      locale_5.hasItem = true;
+      display("You douse the hive with spray until the wasp colony is completely eradicated. You use the machete to cut the hive in half and find a golden key. Gross, but cool. Take it!");
+   }
+   
    function openChest() {
-         display("You open the chest to reveal a year's supply of candy corn! Congratulations, you have claimed the treasure of the Caves of Karkarog.")
+      display("You open the chest to reveal a year's supply of candy corn! Congratulations, you have claimed the treasure of the Caves of Karkarog.")
    }
